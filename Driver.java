ee40832e93222f9c65a856da07c64cba74035654
Dust-rivera <dustine_gian_rivera@dlsu.edu.ph>
2025-06-26 18:32:06 +0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,6 +1,69 @@
+import java.util.Scanner;
+
 public class Driver {
+
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+
+        int timer = 180;
+        int tickTimer = 0;
+        int stringTick = 0;
+
+        User user = new User();
+        Board board = new Board(user);
+
+        Thread inputLoop = new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (board.getRunning()) {
+                if (scanner.hasNextLine()) {
+                    user.setInput(scanner.nextLine());
+                    board.input(board);
+                }
+            }
+            scanner.close();
+        });
+
+        inputLoop.setDaemon(true);
+        inputLoop.start();
+
+        while (board.getRunning()) {
+
+            board.update();
+            if (!board.getRunning())
+                break;
+            System.out.println("Timer: " + (timer) + " seconds");
+
+            tickTimer++;
+
+            board.display();
+
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                break;
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            if (!board.getMessage().equals(""))
+                stringTick++;
+
+            if (stringTick == 4) {
+                board.setMessage("");
+                stringTick = 0;
+            }
+            if (tickTimer % 4 == 0)
+                timer--;
+
+            if (timer == -1) {
+                board.setRunning(false);
+                System.out.println("GAME WON!!!!!");
+            }else if (timer <= 10
+                    && board.getZombieList().isEmpty()
+                    && !board.getfinalWaveFlag()) {
+                board.setRunning(false);
+                System.out.println("GAME WON!!!!!");
+            }
+
+        }
     }
 }
